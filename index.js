c34bfcd0f4f65a5e3280261e452f3da004c4416e
Muhammad Faqih Alharits <aritlhq@gmail.com>
2025-07-08 13:22:35 +0700

    feat: Update Swagger UI path and configuration
    Load API version from environment, update Swagger path.

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
 import {fileURLToPath} from 'url';
+import 'dotenv/config';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const swaggerDocument = YAML.load(path.join(__dirname, 'fadagi-farm.yaml'));
+const apiVersion = process.env.API_VERSION;
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = YAML.load(path.join(__dirname, `${apiVersion}.yaml`));
+
+app.use(`/api/${apiVersion}`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
-    console.log(`Swagger UI running at http://localhost:${port}/docs`);
+    console.log(`Swagger UI running at http://localhost:${port}/api/${apiVersion}`);
 });
